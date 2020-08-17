98c47e8690f4d26101ed729864872f522dd57dd3
sonaspy <sonaspy@zju.edu.cn>
2020-08-17 13:46:47 +0800

    update

---
+++ task.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git filter-branch --env-filter '
+OLD_EMAIL=newguo@sonaspy-hackintosh.lan
+CORRECT_NAME=sonaspy
+CORRECT_EMAIL=sonaspy@zju.edu.cn
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi' --tag-name-filter cat -- --branches --tags
